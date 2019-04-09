5e128cb36cdf2fab4c8c1833a424f9da8b5ad6ca
hearing <1124031072@qq.com>
2019-04-09 10:47:48 +0800

    hhh

---
+++ src/main/java/com/hearing/rire/service/UserService.java
@@ -0,0 +1,15 @@
+package com.hearing.rire.service;
+
+import com.hearing.rire.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Create by hearing on 19-4-9
+ */
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+}
